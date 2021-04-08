e382d55171f9002b628bebed24fb9e11136f5f78
Muli Ann <muliann582@gmail.com>
2021-04-08 11:52:03 +0300

    Challange 2

--- num_guessing.py
+++ num_guessing.py
@@ -1,10 +1,10 @@
 # Number Guessing
 import random
 dice_side=random.randint(1,9)
-num=int(input("Rolls a 9-sided dice.Which number do you think it will be?"))
+num=int(input("Rolls a 9-sided dice.Which number do you think it will be? "))
 while num != dice_side:
     print("well, your answer is incorrect.Here is a new hint for you, if the right side is multiplied by a multiple of 2 the answer is "+str( dice_side*2))    
-    num=int(input("new guess"))
+    num=int(input("new guess: "))
 if num==dice_side:
     print("congrats!!Your guess is correct.")
     
